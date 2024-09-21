2bde86fd7720dcc1a7e7986f016d87073bd6686a
AkashDataScience <akashmshah19@gmail.com>
2024-09-21 13:49:20 +0530

    Added maskin

--- image_finetuning/pretraining/dataset.py
+++ image_finetuning/pretraining/dataset.py
@@ -1,8 +1,6 @@
 import torch
 from torch.utils.data import Dataset
-from PIL import Image
 import random
-import os
 
 class ImageConversationDataset(Dataset):
     def __init__(self, data, tokenizer, max_length=512):
@@ -19,14 +17,25 @@ class ImageConversationDataset(Dataset):
         conversations = item['conversations']
 
         # Randomly select an even-numbered index
-        max_even_index = len(conversations)
-        selected_index = random.randrange(0, max_even_index, 2)
+        max_even_index = len(conversations) - 1
+        selected_index = random.randrange(0, max_even_index + 1, 2)
 
         # Get the human question and GPT response
         human_msg = conversations[selected_index]['value']
         ai_msg = conversations[selected_index + 1]['value']
 
-        input_ids = self.tokenizer.encode(human_msg, max_length=self.max_length, truncation=True, padding='max_length')
-        target_ids = self.tokenizer.encode(ai_msg, max_length=self.max_length, truncation=True, padding='max_length')
+        # Tokenize the conversation
+        tokenized = self.tokenizer(
+            human_msg,
+            ai_msg,
+            truncation=True,
+            max_length=self.max_length,
+            padding='max_length',
+            return_tensors='pt'
+        )
 
-        return image_name, input_ids, target_ids
+        return {
+            'image_name': image_name,
+            'input_ids': tokenized.input_ids.squeeze(),
+            'attention_mask': tokenized.attention_mask.squeeze()
+        }
