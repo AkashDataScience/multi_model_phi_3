f4888d8611fc5c1a7f49139bd1d4018e8a138037
AkashDataScience <akashmshah19@gmail.com>
2024-09-30 20:46:26 +0530

    Reducing max steps

--- image_finetuning/pretraining/main.py
+++ image_finetuning/pretraining/main.py
@@ -16,7 +16,7 @@ MODEL_NAME = "microsoft/phi-3-mini-4k-instruct"
 CLIP_EMBED = 512  # Adjust based on your CLIP model
 PHI_EMBED = 3072  # Adjust based on the phi-3-mini model
 BATCH_SIZE = 8
-MAX_STEPS = 2000
+MAX_STEPS = 1500
 LEARNING_RATE = 5e-5
 WARMUP_STEPS = 100
 
